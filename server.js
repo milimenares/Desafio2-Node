6c7ca33c6f3d07fb3b28be67f95d6ae644a0167c
Melany Menares <menares.melany@gmail.com>
2025-05-31 21:22:15 -0400

    Cambios

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 import express from 'express'
 import 'dotenv/config'
-import { getHtml, getCanciones } from './src/controllers/canciones.controllers.js'
+import { getHtml, getCanciones, crearCanciones } from './src/controllers/canciones.controllers.js'
 
 const app = express()
 
 const PORT = process.env.PORT ?? 3000 // hecho con variables de entorno
 
+app.use(express.json()) // para poder recibir datos en formato JSON
+
 app.get('/', getHtml)
 
-app.get('/repertorio', getCanciones)
+app.get('/canciones', getCanciones)
+
+app.post('/canciones', crearCanciones)
 
 app.listen(PORT, console.log(`Servidor andando http://localhost:${PORT}`))
--- src/controllers/canciones.controllers.js
+++ src/controllers/canciones.controllers.js
@@ -1,18 +1,41 @@
 import fs from 'fs'
 import path from 'path'
+import crypto from 'crypto'
 
 const getHtml = (req, res) => {
-  const filePath = path.resolve('index.html')
+  const filePath = path.resolve('frontend/index.html')
   res.sendFile(filePath)
 }
 
 const getCanciones = (req, res) => {
   try {
-    const repertorio = JSON.parse(fs.readFileSync('repertorio.json', 'utf-8'))
-    res.json(repertorio)
+    const canciones = JSON.parse(fs.readFileSync('repertorio.json', 'utf-8'))
+    res.json(canciones)
   } catch (error) {
     console.error('Error al cargar el repertorio:', error)
   }
 }
 
-export { getHtml, getCanciones }
+const crearCanciones = (req, res) => {
+  try {
+    const { cancion, artista, tono } = req.body
+    const id = crypto.randomUUID()
+    const nuevaCancion = {
+      id,
+      cancion,
+      artista,
+      tono
+    }
+    const canciones = JSON.parse(fs.readFileSync('repertorio.json', 'utf-8'))
+    console.log('Antes:', canciones)
+    canciones.push(nuevaCancion)
+    console.log('Después:', canciones)
+
+    fs.writeFileSync('repertorio.json', JSON.stringify(canciones))
+    res.send('Canción creada con éxito')
+  } catch (error) {
+    console.error('Error al crear canciones', error)
+  }
+}
+
+export { getHtml, getCanciones, crearCanciones }
